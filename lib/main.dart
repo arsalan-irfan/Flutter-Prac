1f682f3e25b3d1b611bc6385ac33993ce371640b
Arsalan <mohdarsalan318@gmail.com>
2019-12-23 00:08:24 +0500

    Fist commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("My First app"),
+      centerTitle: true,
+      backgroundColor: Colors.red[600],
+    ),
+    body:Center(
+      child:Text(
+        "Hello Ninjas!",
+        style: TextStyle(
+          fontSize:20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color:Colors.grey[600],
+          fontFamily: 'IndieFlower'
+           ),
+        )
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child:Text("click"),
+      backgroundColor: Colors.red[600],
+    ),
+  );
+  }
+}

fdb588647aff092b5b73e346d444a69cde087567
Arsalan <mohdarsalan318@gmail.com>
2019-12-27 12:05:50 +0500

    Playing with Row widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home()
-));
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("My First app"),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body:Center(
-      child:Text(
-        "Hello Ninjas!",
-        style: TextStyle(
-          fontSize:20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color:Colors.grey[600],
-          fontFamily: 'IndieFlower'
-           ),
-        )
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child:Text("click"),
-      backgroundColor: Colors.red[600],
-    ),
-  );
+      appBar: AppBar(
+        title: Text("My First app"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Hello World'),
+          FlatButton(
+            onPressed: (){},
+            color:Colors.amber,
+            child:Text('click me')
+          ),
+          Container(
+            padding:EdgeInsets.all(30.0) ,
+            color:Colors.cyan,
+            child:Text('Inside container')
+            )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }
